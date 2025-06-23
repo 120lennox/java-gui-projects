f507b8886bb3a1aee104065b91d3fc15e6dd0d9d
Lennox Mountain <lennoxmountain67@gmail.com>
2025-06-23 17:09:00 +0200

    Added Selection.java with a simple if-else statement.

---
+++ Selection.java
@@ -0,0 +1,12 @@
+public class Selection{
+    public static void main(String[] args) {
+        int temperature = 20;
+
+        if (temperature >= 21){
+            System.out.println("It's warm, put on shorts!");
+        }
+        else{
+            System.out.println("It's cold, put on a jacket!");
+        }
+    }
+}
